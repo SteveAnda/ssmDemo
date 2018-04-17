bcc72b6b2ce9d178762f81ec2d74392cf2797c2a
wanganda <wanganda@topwisesz.com>
2018-04-17 18:34:22 +0800

    create a new branch and git checkout for switch branch

--- base-manage/base-manage-dao/src/main/java/com/dmsd/dao/TUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dmsd.dao;
-
-import com.dmsd.pojo.TUser;
-
-public interface TUserMapper {
-    TUser selectByPrimaryKey(int id);
-}
--- base-manage/base-manage-pojo/src/main/java/com/dmsd/pojo/TUserExample.java
+++
@@ -1,4 +0,0 @@
-package com.dmsd.pojo;
-
-public class TUserExample {
-}
